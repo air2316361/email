eb34db841fedcbcc67cea359a3e87924ed69a753
中国特色社会主义码农 <iam2316361@sina.com>
2026-02-11 15:33:25 +0800

    Debug

--- src/index.js
+++ src/index.js
@@ -10,26 +10,22 @@ export default {
 		const reader = message.raw.getReader();
 		let content = "";
 		function read() {
-			return reader.read().then(({ done, value }) => {
+			return reader.read().then(async ({ done, value }) => {
 				if (!done) {
 					const chunk = decoder.decode(value);
 					content += chunk;
 					return read();
 				}
 				console.log(content);
-				simpleParser(content, async (err, parsed) => {
-					if (err) {
-						return done(err);
-					}
-					console.log(parsed.text);
-					await env.KV.put('email', JSON.stringify({
-						from: parsed.from.text,
-						to: parsed.to.text,
-						subject: parsed.subject,
-						date: Math.floor(parsed.date / 1000),
-						content: parsed.text
-					}));
-				})
+				const parsed = simpleParser(content);
+				console.log(parsed.text);
+				await env.KV.put('email', JSON.stringify({
+					from: parsed.from.text,
+					to: parsed.to.text,
+					subject: parsed.subject,
+					date: Math.floor(parsed.date / 1000),
+					content: parsed.text
+				}));
 			});
 		}
 		await read();
