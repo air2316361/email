0533e54ca6e70badda6d93f41917bbdab9e7e906
中国特色社会主义码农 <iam2316361@sina.com>
2026-02-11 15:26:05 +0800

    Debug

--- src/index.js
+++ src/index.js
@@ -7,31 +7,40 @@ export default {
 		return new Response(value);
 	},
 	async email(message, env, ctx) {
-		const reader = message.raw.getReader();
-		let content = "";
-		function read() {
-			return reader.read().then(({ done, value }) => {
-				if (!done) {
-					const chunk = decoder.decode(value);
-					content += chunk;
-					return read();
-				}
-				console.log(content);
-				simpleParser(content, async (err, parsed) => {
-					if (err) {
-						return done(err);
-					}
-					console.log(parsed.text);
-					await env.KV.put('email', JSON.stringify({
-						from: parsed.from.text,
-						to: parsed.to.text,
-						subject: parsed.subject,
-						date: Math.floor(parsed.date / 1000),
-						content: parsed.text
-					}));
-				})
-			});
-		}
-		await read();
+		// const reader = message.raw.getReader();
+		// let content = "";
+		// function read() {
+		// 	return reader.read().then(({ done, value }) => {
+		// 		if (!done) {
+		// 			const chunk = decoder.decode(value);
+		// 			content += chunk;
+		// 			return read();
+		// 		}
+		// 		console.log(content);
+		// 		simpleParser(content, async (err, parsed) => {
+		// 			if (err) {
+		// 				return done(err);
+		// 			}
+		// 			console.log(parsed.text);
+		// 			await env.KV.put('email', JSON.stringify({
+		// 				from: parsed.from.text,
+		// 				to: parsed.to.text,
+		// 				subject: parsed.subject,
+		// 				date: Math.floor(parsed.date / 1000),
+		// 				content: parsed.text
+		// 			}));
+		// 		})
+		// 	});
+		// }
+		// await read();
+		const email = await simpleParser(message.raw);
+		console.log(email.text);
+		await env.KV.put('email', JSON.stringify({
+			from: email.from.text,
+			to: email.to.text,
+			subject: email.subject,
+			date: Math.floor(email.date.getTime() / 1000),
+			content: email.text
+		}));
 	}
 };
